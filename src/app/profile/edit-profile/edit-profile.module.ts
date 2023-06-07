3e8c6eb9b04122f04cbde197943aba2266239da1
Dhruvi <dhruvi7489@gmail.com>
2023-06-07 11:39:46 +0530

    - remove date format set locale

--- src/app/profile/edit-profile/edit-profile.module.ts
+++ src/app/profile/edit-profile/edit-profile.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -10,18 +10,6 @@ import { EditProfilePage } from './edit-profile.page';
 import { ProfileHeaderPageModule } from '../profile-header/profile-header.module';
 import { CustomButtonModule, CustomInputModule } from 'w4u-custom-components';
 import { DirectivesModule } from 'src/app/directives/directives.module';
-
-const MY_FORMATS = {
-  parse: {
-    dateInput: 'MM/DD/YYYY',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
 
 @NgModule({
   imports: [
@@ -35,11 +23,5 @@ const MY_FORMATS = {
     DirectivesModule
   ],
   declarations: [EditProfilePage],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: MY_FORMATS
-    },
-  ]
 })
 export class EditProfilePageModule { }
